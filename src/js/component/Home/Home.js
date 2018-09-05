a12bd8922e714913123607b3b037555fcbca746e
klareH <klare0216@gmail.com>
2018-09-05 10:10:44 +0800

    implement: user can create new tags

--- src/js/component/Home/Home.js
+++ src/js/component/Home/Home.js
@@ -6,24 +6,25 @@ import TagsBoard from './TagsBoard';
 import NoteInputBoard from './NoteInputBoard';
 
 class Home extends React.Component {
-	
+
 	constructor(props) {
 		super(props);
 		this.state = {
 			fields: [
-				{ id: 0, display: '-', plant: 'empty', note: ''},
-				{ id: 1, display: '-', plant: 'empty', note: ''},
-				{ id: 2, display: '-', plant: 'empty', note: ''},
-				{ id: 3, display: '-', plant: 'empty', note: ''},
-				{ id: 4, display: '-', plant: 'empty', note: ''},
-				{ id: 5, display: '-', plant: 'empty', note: ''},
-				{ id: 6, display: '-', plant: 'empty', note: ''},
-				{ id: 7, display: '-', plant: 'empty', note: ''},
+				{ id: 0, display: '-', plant: 'empty', note: '' },
+				{ id: 1, display: '-', plant: 'empty', note: '' },
+				{ id: 2, display: '-', plant: 'empty', note: '' },
+				{ id: 3, display: '-', plant: 'empty', note: '' },
+				{ id: 4, display: '-', plant: 'empty', note: '' },
+				{ id: 5, display: '-', plant: 'empty', note: '' },
+				{ id: 6, display: '-', plant: 'empty', note: '' },
+				{ id: 7, display: '-', plant: 'empty', note: '' },
 			],
 			tags: [
-				{ id: 0, text: '讀書'},
-				{ id: 1, text: '練琴'},
+				{ id: 0, text: '讀書' },
+				{ id: 1, text: '練琴' },
 			],
+			newTag: '',
 			seconds: 0,
 			timer: 0,
 			plantingField: -1,
@@ -35,18 +36,19 @@ class Home extends React.Component {
 	}
 
 	componentDidUpdate() {
-		if(this.isCountDownJustEnded()) {
+		if (this.isCountDownJustEnded()) {
 			const plant = this.plants[Math.floor((Math.random() * 10) % 3)];
-			this.setField((field, state) => { 
-				field.plant = plant; 
-				field.display = state.doing;});
-			this.setState({isShowNoteInputBoard: true});
+			this.setField((field, state) => {
+				field.plant = plant;
+				field.display = state.doing;
+			});
+			this.setState({ isShowNoteInputBoard: true });
 			this.stopTimer();
 		}
 	}
-		
-	setField( settingFunction, id = -1 ) {
-		this.setState( preState => {
+
+	setField(settingFunction, id = -1) {
+		this.setState(preState => {
 			let newFields = [...preState.fields];
 			const index = (id == -1) ? preState.plantingField : id;
 			settingFunction(newFields[index], preState);
@@ -62,54 +64,71 @@ class Home extends React.Component {
 		return (
 			<div >
 				<HeadBar />
-				<Farm week={ this.state.fields }
-					onClickCheckBox={ id => this.onClickField(id)}/>
-				{ this.getTimerBoard() }
-				{ this.getTagsBoard() }
-				{ this.getNoteInputBoard() }
+				<Farm week={this.state.fields}
+					onClickCheckBox={id => this.onClickField(id)} />
+				{this.getTimerBoard()}
+				{this.getTagsBoard()}
+				{this.getNoteInputBoard()}
 			</div>
 		);
 	}
 
 	getTimerBoard() {
-		if(this.isTimerStart())
-			return(
-				<TimerBoard time={ this.getMinutesAndSeconds() }
-					onClick={ () => this.onClickToCancelPlanting() } />
+		if (this.isTimerStart())
+			return (
+				<TimerBoard time={this.getMinutesAndSeconds()}
+					onClick={() => this.onClickToCancelPlanting()} />
 			);
 		return '';
 	}
 
 	getTagsBoard() {
-		if(this.state.isShowTagsBoard)
-			return(
-				<TagsBoard onClickTag={ tag => this.onClickTag(tag) } tags={ this.state.tags }/>
+		if (this.state.isShowTagsBoard)
+			return (
+				<TagsBoard
+					onChangeInput={ event => this.onChangeInput(event) }
+					onClickEnter={ () => this.createNewTag() }
+					onClickTag={tag => this.onClickTag(tag)}
+					tags={this.state.tags} />
 			);
 		return '';
 	}
 
+	onChangeInput(event) {
+		this.setState({ newTag: event.target.value });
+	}
+
+	createNewTag() {
+		this.setState( preState => {
+			let newTags = [...preState.tags];
+			newTags.push({ id: newTags.length, text: preState.newTag });
+			return { tags: newTags };
+		});
+		this.onClickTag('# ' + this.state.newTag);
+	}
+
 	onClickTag(tagName) {
-		this.setField( field => { field.display += " " + tagName;});
+		this.setField(field => { field.display += " " + tagName; });
 		this.setState({ doing: tagName, isShowTagsBoard: false });
 		this.startTimer();
 	}
 
 	getNoteInputBoard() {
-		if(this.state.isShowNoteInputBoard)
-			return(
-				<NoteInputBoard onClick={ text => this.onClickEnter(text) }/>
+		if (this.state.isShowNoteInputBoard)
+			return (
+				<NoteInputBoard onClick={text => this.onClickEnter(text)} />
 			);
 		return '';
 	}
 
 	onClickEnter(text) {
-		this.setField( fields => { fields.note = text; } );
-		this.setState({ isShowNoteInputBoard: false , plantingField: -1 });
+		this.setField(fields => { fields.note = text; });
+		this.setState({ isShowNoteInputBoard: false, plantingField: -1 });
 	}
 
 
 	onClickField(id) {
-		if(this.isAbleToPlant(id)) {
+		if (this.isAbleToPlant(id)) {
 			this.plantOnTheField(id);
 			this.setState({ isShowTagsBoard: true });
 		}
@@ -121,22 +140,22 @@ class Home extends React.Component {
 
 	plantOnTheField(id) {
 		// this.displayMessageOnTheFieldWithId('種植中', id);
-		this.setField( field => { field.display = '種植中'; console.log('hi')} , id);
+		this.setField(field => { field.display = '種植中'; console.log('hi') }, id);
 
 		this.setState({ plantingField: id });
 	}
 
-	isTheFieldPlanted(id){
+	isTheFieldPlanted(id) {
 		return this.state.fields[id].display !== '-';
 	}
 
 	startTimer() {
 		const timer = setInterval(() => this.decreaseTime(), 1000);
-		this.setState({ timer: timer, seconds: 1});
+		this.setState({ timer: timer, seconds: 1 });
 	}
 
 	decreaseTime() {
-		this.setState((prevState) => ({ seconds :prevState.seconds - 1 }));
+		this.setState((prevState) => ({ seconds: prevState.seconds - 1 }));
 	}
 
 	getMinutesAndSeconds() {
@@ -145,24 +164,24 @@ class Home extends React.Component {
 	}
 
 	onClickToCancelPlanting() {
-		if(this.isPlanting()) {
+		if (this.isPlanting()) {
 			this.stopTimer();
 			this.clearThePlantingField();
 		}
 	}
 
-	clearThePlantingField(){
-		this.setField( field => { field.display = '-';});
+	clearThePlantingField() {
+		this.setField(field => { field.display = '-'; });
 		this.setState({ plantingField: -1 });
 	}
 
 	displayMessageOnTheFieldWithId(message, id) {
 		console.log(id);
-		this.setState( preState => this.getFieldsWithUpdateDisplay(preState.fields, id, message));
+		this.setState(preState => this.getFieldsWithUpdateDisplay(preState.fields, id, message));
 	}
 
 	getFieldsWithUpdateDisplay(fields, plantingField, display) {
-		if(plantingField >= 0) {
+		if (plantingField >= 0) {
 			let newFields = [...fields];
 			newFields[plantingField].display = display;
 			return { fields: newFields };
@@ -183,7 +202,7 @@ class Home extends React.Component {
 	}
 
 	componentWillUnmount() {
-		if(this.state.timer != 0)
+		if (this.state.timer != 0)
 			clearInterval(this.state.timer);
 	}
 
--- src/js/component/Home/TagsBoard.js
+++ src/js/component/Home/TagsBoard.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import Tag from './Tag';
 
-const TagsBoard = props => {
-	const render = props.tags.map( tag => {
-		return (
-			<Tag key={ tag.id }
-				text={ tag.text }
-				onClick={ props.onClickTag } />
-		);
-	});
+class TagsBoard extends React.Component {
 
-	return(
-		<div>
-			<p>你接下來要做什麼努力呢？</p>
-			<ul>
-				{ render }
-			</ul>
-		</div>
-	);
+	constructor(props) {
+		super(props);
+		this.state = {
+			isShowCreatNewTag: false,
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<p>你接下來要做什麼努力呢？</p>
+				<ul>
+					{this.getTags()}
+				</ul>
+				<button onClick={() => this.setState({ isShowCreatNewTag: true })}>+</button>
+				{this.getInsertNewTagsInput()}
+			</div>
+		);
+	}
+
+	getTags() {
+		return this.props.tags.map(tag => {
+			return (
+				<Tag key={tag.id}
+					text={tag.text}
+					onClick={this.props.onClickTag} />
+			);
+		});
+	}
+
+	getInsertNewTagsInput() {
+		if (this.state.isShowCreatNewTag) {
+			return (
+				<div>
+					<input onChange={ this.props.onChangeInput } type='text' />
+					<button onClick={ this.props.onClickEnter }>Enter</button>
+				</div>
+			);
+		}
+	}
+	
+
+
 };
 
 export default TagsBoard;
